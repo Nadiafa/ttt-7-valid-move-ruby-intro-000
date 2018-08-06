51657f2fc616b9603019c8f0a0e72525584f86b1
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-06 10:54:56 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -13,3 +13,12 @@ def position_taken?(board, index)
   board[index] =='X' || board[index] =='O'
 end
 
+
+
+
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def position_taken?(board, index)
+  board[index] != " "
+end
