15b5dc3749b9d62fa14977fe7dafaf447a4b824a
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-06 10:55:57 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,16 +8,6 @@ end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
-def position_taken?(board, index)
-  board[index] =='' || board[index] ==' '|| board[index] == nil
-  board[index] =='X' || board[index] =='O'
-end
-
-
-
-
-  index.between?(0,8) && !position_taken?(board, index)
-end
 
 def position_taken?(board, index)
   board[index] != " "
